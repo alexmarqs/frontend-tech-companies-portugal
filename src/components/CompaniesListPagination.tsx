68208e8df5d00f67e536fa906c3dcbee539201fd
Alexandre Marques <pt.alexandremarques@gmail.com>
2024-02-17 17:00:04 +0000

    chore: add new pagination ux, ui

--- src/components/CompaniesListPagination.tsx
+++ src/components/CompaniesListPagination.tsx
@@ -1,6 +1,11 @@
 import { SearchParams } from "@/lib/types";
 import { cn } from "@/lib/utils";
-import { ChevronLeft, ChevronRight } from "lucide-react";
+import {
+  ChevronLeft,
+  ChevronRight,
+  ChevronsLeft,
+  ChevronsRight,
+} from "lucide-react";
 import Link from "next/link";
 import { Button } from "./ui/button";
 
@@ -45,9 +50,20 @@ export default function CompaniesListPagination({
           size="sm"
           asChild
         >
-          <Link href={createPageUrl(currentPage - 1)}>
+          <Link className="!px-2" href={createPageUrl(1)}>
+            <ChevronsLeft className="shrink-0" size={16} />
+          </Link>
+        </Button>
+        <Button
+          className={cn(
+            isPreviousDisabled && "pointer-events-none text-muted-foreground",
+          )}
+          variant="outline"
+          size="sm"
+          asChild
+        >
+          <Link className="!px-2" href={createPageUrl(currentPage - 1)}>
             <ChevronLeft className="shrink-0" size={16} />
-            Previous
           </Link>
         </Button>
         <Button
@@ -58,9 +74,20 @@ export default function CompaniesListPagination({
             isNextDisabled && "pointer-events-none text-muted-foreground",
           )}
         >
-          <Link href={createPageUrl(currentPage + 1)}>
-            Next
+          <Link className="!px-2" href={createPageUrl(currentPage + 1)}>
             <ChevronRight className="shrink-0" size={16} />
+          </Link>
+        </Button>
+        <Button
+          variant="outline"
+          asChild
+          size="sm"
+          className={cn(
+            isNextDisabled && "pointer-events-none text-muted-foreground",
+          )}
+        >
+          <Link className="!px-2" href={createPageUrl(totalPages)}>
+            <ChevronsRight className="shrink-0" size={16} />
           </Link>
         </Button>
       </div>
