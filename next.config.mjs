90bb7f86acf1b05316459b561d539342a6726466
Alexandre Marques <pt.alexandremarques@gmail.com>
2024-02-25 17:52:29 +0000

    chore: add explanation to warning setting due to usesearchparams

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
+    // as my companies list components are client side anyway, i want to avoid a flickering due to the suspense fallback
     missingSuspenseWithCSRBailout: false,
   },
 };
