42cacb93bc4f31dd3492630bc3a1136833116bc3
Alexandre Marques <pt.alexandremarques@gmail.com>
2024-03-08 13:35:02 +0000

    chore: refactorings

--- src/lib/actions/companies.ts
+++ src/lib/actions/companies.ts
@@ -1,24 +1,28 @@
 "use server";
 
 import { unstable_cache as cache } from "next/cache";
+import { cache as dedup } from "react";
 import { parseCompaniesData } from "../parser";
 
-export const getParsedCompaniesData = cache(
-  async () => {
-    const data = await parseCompaniesData();
-    const updatedAtISODate = new Date().toISOString();
+export const getParsedCompaniesData = dedup(
+  cache(
+    async () => {
+      const data = await parseCompaniesData();
+      const updatedAtISODate = new Date().toISOString();
 
-    return {
-      companies: data.companies,
-      availableLocations: data.availableLocations,
-      availableCategories: data.availableCategories,
-      updatedAtISODate,
-    };
-  },
-  ["companies"],
-  {
-    revalidate: 21600, // 6 hours
-  },
+      return {
+        companies: data.companies,
+        availableLocations: data.availableLocations,
+        availableCategories: data.availableCategories,
+        updatedAtISODate,
+      };
+    },
+    ["companies"],
+    {
+      tags: ["companies"],
+      revalidate: 60 * 60 * 6, // 6 hours
+    },
+  ),
 );
 
 export const getParsedCompanyBySlug = async (slug: string) => {
