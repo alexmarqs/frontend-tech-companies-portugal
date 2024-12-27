007897ab8bb4ba09ef5495cdcbccefe72c0f88a2
Alexandre Marques <pt.alexandremarques@gmail.com>
2024-12-27 12:51:11 +0000

    chore: replace tremor by charts; use biome instead of prettier/eslint;

--- src/components/DashboardAnalytics.tsx
+++ src/components/DashboardAnalytics.tsx
@@ -1,13 +1,49 @@
 "use client";
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
 import { PageViewsData } from "@/lib/types";
-import { AreaChart, BarList, Card, Metric, Text, Title } from "@tremor/react";
+import {
+  Area,
+  AreaChart,
+  Bar,
+  BarChart,
+  CartesianGrid,
+  LabelList,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 type DashboardAnalyticsProps = {
   timeSeriesData: PageViewsData;
   totalViews: number;
   topPages: { name: string; value: number }[];
 };
+
+const chartConfig = {
+  views: {
+    label: "Views",
+    color: "hsl(var(--chart-1))",
+  },
+  name: {
+    color: "hsl(var(--background))",
+  },
+  value: {
+    label: "Views",
+    color: "hsl(var(--chart-1))",
+  },
+} satisfies ChartConfig;
 
 export const DashboardAnalytics = ({
   timeSeriesData,
@@ -17,26 +53,102 @@ export const DashboardAnalytics = ({
   return (
     <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
       <Card className="col-span-3">
-        <Title>Views @ 15 days</Title>
-        <div className="flex items-baseline gap-2">
-          <Metric>{totalViews}</Metric>
-          <Text>Total views</Text>
-        </div>
-        <AreaChart
-          index="time"
-          colors={["blue"]}
-          yAxisWidth={30}
-          className="mt-3 h-72"
-          data={timeSeriesData.map((data) => ({
-            time: new Date(data.time).toISOString().split("T")[0],
-            views: Object.values(data.views).reduce((a, b) => a + b, 0),
-          }))}
-          categories={["views"]}
-        />
+        <CardHeader>
+          <CardTitle>Views (15 days)</CardTitle>
+          <CardDescription>{`Total views: ${totalViews}`}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <ChartContainer config={chartConfig}>
+            <AreaChart
+              accessibilityLayer
+              data={timeSeriesData.map((data) => ({
+                time: new Date(data.time).toISOString().split("T")[0],
+                views: Object.values(data.views).reduce((a, b) => a + b, 0),
+              }))}
+              margin={{
+                left: 12,
+                right: 12,
+              }}
+            >
+              <CartesianGrid vertical={false} />
+              <XAxis
+                dataKey="time"
+                tickLine={false}
+                axisLine={false}
+                tickMargin={8}
+              />
+              <YAxis
+                dataKey="views"
+                tickLine={false}
+                axisLine={false}
+                tickMargin={8}
+              />
+              <ChartTooltip
+                cursor={false}
+                content={<ChartTooltipContent indicator="dot" hideLabel />}
+              />
+              <Area
+                dataKey="views"
+                type="linear"
+                fill="var(--color-views)"
+                fillOpacity={0.4}
+                stroke="var(--color-views)"
+              />
+            </AreaChart>
+          </ChartContainer>
+        </CardContent>
       </Card>
-      <Card className="col-span-3 h-[20rem]">
-        <Title>Top 5 pages views @ 15 days</Title>
-        <BarList className="mt-5" data={topPages} />
+      <Card className="col-span-3">
+        <CardHeader>
+          <CardTitle>Top 5 pages views (15 days)</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ChartContainer className="h-[20rem] w-full" config={chartConfig}>
+            <BarChart
+              accessibilityLayer
+              data={topPages}
+              layout="vertical"
+              margin={{
+                right: 20,
+              }}
+            >
+              <YAxis
+                dataKey="name"
+                type="category"
+                tickLine={false}
+                tickMargin={10}
+                axisLine={false}
+                hide
+              />
+              <XAxis dataKey="value" type="number" hide />
+              <ChartTooltip
+                cursor={false}
+                content={<ChartTooltipContent indicator="line" />}
+              />
+              <Bar
+                dataKey="value"
+                layout="vertical"
+                fill="var(--color-views)"
+                radius={4}
+              >
+                <LabelList
+                  dataKey="name"
+                  position="insideLeft"
+                  offset={8}
+                  className="fill-[--color-name]"
+                  fontSize={12}
+                />
+                <LabelList
+                  dataKey="value"
+                  position="right"
+                  offset={8}
+                  className="fill-foreground"
+                  fontSize={12}
+                />
+              </Bar>
+            </BarChart>
+          </ChartContainer>
+        </CardContent>
       </Card>
     </div>
   );
