5768b2ad42972f75381353130c41cb715aff82cf
Alexandre Marques <pt.alexandremarques@gmail.com>
2025-01-25 09:10:24 +0000

    chore: add shared package - tailwind config

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,21 +1,9 @@
+import baseConfig from "@tech-companies-portugal/tailwind";
 import type { Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
 const config = {
-  content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
-  ],
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
@@ -64,6 +52,7 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config;
+  presets: [baseConfig],
+} satisfies Pick<Config, "theme" | "plugins" | "presets">;
 
 export default config;
---
+++ tooling/tailwind/tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+
+const config = {
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
+
+export default config;
